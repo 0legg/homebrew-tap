c565ba6b8584ed2a855dbae2cab8629afff0319a
Oleg Godovykh <oleg@squareup.com>
2019-07-23 11:37:51 -0700

    Add ktlint 0.33.0

---
+++ Formula/ktlint@0.33.0.rb
@@ -0,0 +1,23 @@
+class KtlintAT0330 < Formula
+  desc "Anti-bikeshedding Kotlin linter with built-in formatter"
+  homepage "https://ktlint.github.io/"
+  url "https://github.com/pinterest/ktlint/releases/download/0.33.0/ktlint"
+  sha256 "2b8487d635977bd844ea22478c457f008fc6732a4257640de2add5e54f23d087"
+
+  bottle :unneeded
+
+  def install
+    bin.install "ktlint"
+  end
+
+  test do
+    (testpath/"In.kt").write <<~EOS
+      fun main( )
+    EOS
+    (testpath/"Out.kt").write <<~EOS
+      fun main()
+    EOS
+    system bin/"ktlint", "-F", "In.kt"
+    assert_equal shell_output("cat In.kt"), shell_output("cat Out.kt")
+  end
+end
